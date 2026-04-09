66df05f44466dabe8d2a479d6123ea19367e871e
Molly <mollymayanderson@hotmail.com>
2026-04-09 01:55:00 -0500

    huh

--- js/main_with_debug.js
+++ js/main_with_debug.js
@@ -33,7 +33,7 @@ function makeMap(topoData, csvData) {
     // convert TopoJSON to GeoJSON
     var geojson = topojson.feature(
         topoData,
-        topoData.objects.HungaryCounties
+        topoData.objects.ne_10m_admin_1_states_provinces
     );
 	console.log("GeoJSON:", geojson);
  
